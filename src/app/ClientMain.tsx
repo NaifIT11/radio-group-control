baaa273294f65f9545f1975ca23f72dbc69a718f
Naif Otibi <nn.n@outlook.cl>
2024-10-25 04:18:10 +0300

    making main header component

--- src/app/ClientMain.tsx
+++ src/app/ClientMain.tsx
@@ -3,10 +3,22 @@
 export default function Main() {
   return (
     <div className="flex flex-col gap-3 rounded border border-indigo-500 p-4">
-      <div className="space-y-2">
-        <h1 className="text-xl">Main Form</h1>
-        <p className="text-sm">choose a one of the following</p>
-      </div>
+        <MainHeader title="Main Form" description="choose one of the follwing" />
+    </div>
+  );
+}
+
+function MainHeader({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-xl">Main Form</h1>
+      <p className="text-sm">choose a one of the following</p>
     </div>
   );
 }
