df01ba2aa454d464748369f47e0795d5046544b4
Naif Otibi <nn.n@outlook.cl>
2024-10-25 04:24:13 +0300

    adding the radio group list

--- src/app/ClientMain.tsx
+++ src/app/ClientMain.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 export default function Main() {
+  const radioOptions = ["google" , "amazon" , "microsoft"]
   return (
     <div className="flex flex-col gap-3 rounded border border-indigo-500 p-4">
         <MainHeader title="Main Form" description="choose one of the follwing" />
+        <div className="flex flex-col gap-3">
+            {radioOptions.map((radioOption => (
+                <div className="flex justify-between items-center">
+                    <label htmlFor="option">{radioOption}</label>
+                    
+                </div>
+            )))}
+        </div>
     </div>
   );
 }
