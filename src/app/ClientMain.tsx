03778d317e2f91b5e924b3ad1781476f13643d83
Naif Otibi <nn.n@outlook.cl>
2024-10-25 04:15:23 +0300

    main header before make it in seperate component

--- src/app/ClientMain.tsx
+++ src/app/ClientMain.tsx
@@ -1,12 +1,12 @@
-"use client"
+"use client";
 
-
-
-
-export default function Main(){
-    return (
-        <div className="flex flex-col gap-3 rounded border p-4">
-
-        </div>
-    )
+export default function Main() {
+  return (
+    <div className="flex flex-col gap-3 rounded border border-indigo-500 p-4">
+      <div className="space-y-2">
+        <h1 className="text-xl">Main Form</h1>
+        <p className="text-sm">choose a one of the following</p>
+      </div>
+    </div>
+  );
 }
